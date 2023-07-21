676a81e829c18dbaae826cea5449ff937b158df2
Adhil <adhil.mhdk28@gmail.com>
2023-07-21 18:27:57 +0530

    react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom"
+import Auth from "./App/Auth/Auth";
+import { useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const router = useLocation()
+  const nav = useNavigate()
+
+  useEffect(() => {
+    if (router.pathname != '/auth') {
+      if (localStorage.getItem('token')==null) {
+        nav('/auth')
+      }
+    }
+  }, [router.pathname])
+
+  return <>
+    <Routes>
+      <Route path="/auth" Component={Auth} />
+    </Routes>
+  </>
 }
 
 export default App;
